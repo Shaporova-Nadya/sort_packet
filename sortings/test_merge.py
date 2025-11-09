94266ec1d892b9f25fc0c62ebd5943d0be12af23
Nadya Shaporova <saporovanada@gmail.com>
2025-11-10 00:40:10 +0300

    Add merge-sort tests

---
+++ sortings/test_merge.py
@@ -0,0 +1,18 @@
+import pytest
+from merge import merge_sort
+
+
+def test_merge():
+    assert merge_sort([2, 99, 4, 1]) == [1, 2, 4, 99]
+
+
+def test_merge_empty():
+    assert merge_sort([]) == []
+
+
+def test_merge_sorted():
+    assert merge_sort([1, 2, 3, 4]) == [1, 2, 3, 4]
+
+
+def test_merge_big_num():
+    assert merge_sort([665, 434, 999, 778]) == [434, 665, 778, 999]
